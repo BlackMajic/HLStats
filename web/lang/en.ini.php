551ccfcbd17d7993333b4d24766fd03602ae3bdd
jumpin-banana <jumpin.banana@gmail.com>
2009-07-08 12:36:59 +0200

    default translation file

---
+++ web/lang/en.ini.php
@@ -0,0 +1,11 @@
+; this is not a php file
+; it is a ini file
+;
+; key = value;
+;
+; quotes are optional (?!)
+
+Player Rankings... = Player Rankings...;
+Sections = Sections;
+Clan Rankings... = Clan Rankings...;
+Weapon Statistics... = Weapon Statistics...;
