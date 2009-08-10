c9f7d1700efb899a6a74f3c52d162a35746396a0
jumpin-banana <jumpin.banana@gmail.com>
2009-08-10 09:28:18 +0200

    typo

--- daemon/hlstats-awards.pl
+++ daemon/hlstats-awards.pl
@@ -165,7 +165,7 @@ print "-- Connecting to MySQL database '$db_name' on '$db_host' as user '$db_use
 
 $db_conn = DBI->connect(
 	"DBI:mysql:$db_name:$db_host",
-	$db_user, $db_pass, {RaiseError => 1,'mysql_enable_utf8' => 1, 'mysql_auto_reconnect' => 1
+	$db_user, $db_pass, {RaiseError => 1,'mysql_enable_utf8' => 1, 'mysql_auto_reconnect' => 1,
 				'ShowErrorStatement' => 1 }
 ) or die ("Can't connect to MySQL database '$db_name' on '$db_host'\n" .
 	"$DBI::errstr\n");
