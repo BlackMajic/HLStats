b951a5bd37b8450e101ec154afaf8e2e2afbf902
jumpin-banana <jumpin.banana@gmail.com>
2010-03-04 17:45:09 +0100

    update sql file fixed

--- upgrade/upgrade_from_1.50.sql
+++ upgrade/upgrade_from_1.50.sql
@@ -1,16 +1,27 @@
-ALTER TABLE hlstats_Events_Frags ADD INDEX ( victimId );
-ALTER TABLE hlstats_Events_Frags ADD INDEX ( killerId );
-ALTER TABLE hlstats_Events_Frags ADD INDEX ( weapon );
-ALTER TABLE hlstats_Weapons ADD INDEX ( code );
-ALTER TABLE `hlstats_Events_Frags` ADD INDEX ( `map` )  ;
-ALTER TABLE `hlstats_Players` ADD `skillchangeDate` INT( 10 ) NULL AFTER `oldSkill` ;
-ALTER TABLE `hlstats_Players` ADD `active` INT( 1 ) NOT NULL DEFAULT '1' AFTER `skillchangeDate` ;
-ALTER TABLE `hlstats_Players` ADD INDEX ( `active` ) ;
-ALTER TABLE `hlstats_Players` ADD INDEX ( `hideranking` );
-ALTER TABLE `hlstats_Events_PlayerActions` ADD INDEX ( `actionId` );
-ALTER TABLE `hlstats_Events_PlayerActions` ADD INDEX ( `playerId` );
-ALTER TABLE `hlstats_Events_PlayerActions` ADD INDEX ( `serverId` );
-ALTER TABLE `hlstats_Events_PlayerPlayerActions` ADD INDEX ( `serverId` );
-ALTER TABLE `hlstats_Events_PlayerPlayerActions` ADD INDEX ( `playerId` );
-ALTER TABLE `hlstats_Events_PlayerPlayerActions` ADD INDEX ( `victimId` );
-ALTER TABLE `hlstats_Events_PlayerPlayerActions` ADD INDEX ( `actionId` );
+#
+# HLStats Database Upgrade file
+# -----------------------------
+#
+# REPLACE #DB_PREFIX# WITH YOUR CURRENT HLSTATS PREFIX eg. hlstats
+#
+# To upgrade an existing HLStats 1.50 database to version 1.51:
+#
+#   mysql hlstats_db_name < upgrade_from_1.50.sql
+#
+
+ALTER TABLE `#DB_PREFIX#_Events_Frags` ADD INDEX ( victimId );
+ALTER TABLE `#DB_PREFIX#_Events_Frags` ADD INDEX ( killerId );
+ALTER TABLE `#DB_PREFIX#_Events_Frags` ADD INDEX ( weapon );
+ALTER TABLE `#DB_PREFIX#_Weapons` ADD INDEX ( code );
+ALTER TABLE `#DB_PREFIX#_Events_Frags` ADD INDEX ( `map` )  ;
+ALTER TABLE `#DB_PREFIX#_Players` ADD `skillchangeDate` INT( 10 ) NULL AFTER `oldSkill` ;
+ALTER TABLE `#DB_PREFIX#_Players` ADD `active` INT( 1 ) NOT NULL DEFAULT '1' AFTER `skillchangeDate` ;
+ALTER TABLE `#DB_PREFIX#_Players` ADD INDEX ( `active` ) ;
+ALTER TABLE `#DB_PREFIX#_Players` ADD INDEX ( `hideranking` );
+ALTER TABLE `#DB_PREFIX#_Events_PlayerActions` ADD INDEX ( `actionId` );
+ALTER TABLE `#DB_PREFIX#_Events_PlayerActions` ADD INDEX ( `playerId` );
+ALTER TABLE `#DB_PREFIX#_Events_PlayerActions` ADD INDEX ( `serverId` );
+ALTER TABLE `#DB_PREFIX#_Events_PlayerPlayerActions` ADD INDEX ( `serverId` );
+ALTER TABLE `#DB_PREFIX#_Events_PlayerPlayerActions` ADD INDEX ( `playerId` );
+ALTER TABLE `#DB_PREFIX#_Events_PlayerPlayerActions` ADD INDEX ( `victimId` );
+ALTER TABLE `#DB_PREFIX#_Events_PlayerPlayerActions` ADD INDEX ( `actionId` );
