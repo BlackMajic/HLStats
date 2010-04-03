b647c5467822a893004e458a3aabf73b3b053235
Banana <jumpin.banana@gmail.com>
2010-04-03 17:55:38 +0200

    clan overview

--- web/hlstatsinc/clans.inc.php
+++ web/hlstatsinc/clans.inc.php
@@ -79,7 +79,7 @@ if (isset($_GET["page"])) {
  * @global string $sort
  * @name $sort
  */
-$sort = 'obj_count';
+$sort = 'skill';
 if (isset($_GET["sort"])) {
 	$check = validateInput($_GET['sort'],'nospace');
 	if($check === true) {
@@ -137,8 +137,8 @@ $queryStr = "SELECT SQL_CALC_FOUND_ROWS
 		ROUND(AVG(".DB_PREFIX."_Players.skill)) AS skill,
 		IFNULL(SUM(".DB_PREFIX."_Players.kills)/SUM(".DB_PREFIX."_Players.deaths), '-') AS kpd
 	FROM ".DB_PREFIX."_Clans
-	LEFT JOIN ".DB_PREFIX."_Players ON
-		".DB_PREFIX."_Players.clan=".DB_PREFIX."_Clans.clanId
+	LEFT JOIN ".DB_PREFIX."_Players
+		ON ".DB_PREFIX."_Players.clan=".DB_PREFIX."_Clans.clanId
 	WHERE ".DB_PREFIX."_Clans.game='".mysql_escape_string($game)."'
 		AND ".DB_PREFIX."_Players.hideranking = 0
 	GROUP BY ".DB_PREFIX."_Clans.clanId
@@ -170,7 +170,7 @@ mysql_freeresult($query);
  */
 $query = mysql_query("SELECT FOUND_ROWS() AS 'rows'");
 $result = mysql_fetch_assoc($query);
-$actions['pages'] = (int)ceil($result['rows']/50);
+$clans['pages'] = (int)ceil($result['rows']/50);
 mysql_freeresult($query);
 
 pageHeader(
@@ -205,134 +205,129 @@ pageHeader(
 		<tr>
 			<th class="<?php echo $rcol; ?>"><?php echo l('Rank'); ?></th>
 			<th class="<?php echo $rcol; ?>">
-				<a href="index.php?<?php echo makeQueryString(array('sort'=>'code','sortorder'=>$newSort)); ?>">
-					<?php echo l('Action'); ?>
+				<a href="index.php?<?php echo makeQueryString(array('sort'=>'name','sortorder'=>$newSort)); ?>">
+					<?php echo l('Name'); ?>
 				</a>
-				<?php if($sort == "code") { ?>
+				<?php if($sort == "name") { ?>
+				<img src="<?php echo $g_options["imgdir"]; ?>/<?php echo $sortorder; ?>.gif" alt="Sorting" width="7" height="7" />
+				<?php } ?>
+			</th>
+			<th class="<?php echo $rcol; ?>">
+				<a href="index.php?<?php echo makeQueryString(array('sort'=>'tag','sortorder'=>$newSort)); ?>">
+					<?php echo l('Tag'); ?>
+				</a>
+				<?php if($sort == "tag") { ?>
+				<img src="<?php echo $g_options["imgdir"]; ?>/<?php echo $sortorder; ?>.gif" alt="Sorting" width="7" height="7" />
+				<?php } ?>
+			</th>
+			<th class="<?php echo $rcol; ?>">
+				<a href="index.php?<?php echo makeQueryString(array('sort'=>'skill','sortorder'=>$newSort)); ?>">
+					<?php echo l('Points'); ?>
+				</a>
+				<?php if($sort == "skill") { ?>
+				<img src="<?php echo $g_options["imgdir"]; ?>/<?php echo $sortorder; ?>.gif" alt="Sorting" width="7" height="7" />
+				<?php } ?>
+			</th>
+			<th class="<?php echo $rcol; ?>">
+				<a href="index.php?<?php echo makeQueryString(array('sort'=>'nummembers','sortorder'=>$newSort)); ?>">
+					<?php echo l('Members'); ?>
+				</a>
+				<?php if($sort == "nummembers") { ?>
+				<img src="<?php echo $g_options["imgdir"]; ?>/<?php echo $sortorder; ?>.gif" alt="Sorting" width="7" height="7" />
+				<?php } ?>
+			</th>
+			<th class="<?php echo $rcol; ?>">
+				<a href="index.php?<?php echo makeQueryString(array('sort'=>'kills','sortorder'=>$newSort)); ?>">
+					<?php echo l('Kills'); ?>
+				</a>
+				<?php if($sort == "kills") { ?>
+				<img src="<?php echo $g_options["imgdir"]; ?>/<?php echo $sortorder; ?>.gif" alt="Sorting" width="7" height="7" />
+				<?php } ?>
+			</th>
+			<th class="<?php echo $rcol; ?>">
+				<a href="index.php?<?php echo makeQueryString(array('sort'=>'deaths','sortorder'=>$newSort)); ?>">
+					<?php echo l('Deaths'); ?>
+				</a>
+				<?php if($sort == "deaths") { ?>
+				<img src="<?php echo $g_options["imgdir"]; ?>/<?php echo $sortorder; ?>.gif" alt="Sorting" width="7" height="7" />
+				<?php } ?>
+			</th>
+			<th class="<?php echo $rcol; ?>">
+				<a href="index.php?<?php echo makeQueryString(array('sort'=>'kpd','sortorder'=>$newSort)); ?>">
+					<?php echo l('Kills per Death'); ?>
+				</a>
+				<?php if($sort == "kpd") { ?>
 				<img src="<?php echo $g_options["imgdir"]; ?>/<?php echo $sortorder; ?>.gif" alt="Sorting" width="7" height="7" />
 				<?php } ?>
 			</th>
 		</tr>
+		<?php
+		if(!empty($clans['data'])) {
+			if($page > 1) {
+				$rank = ($page - 1) * (50 + 1);
+			}
+			else {
+				$rank = 1;
+			}
+			foreach($clans['data'] as $k=>$entry) {
+				toggleRowClass($rcol);
+
+				echo '<tr>',"\n";
+
+				echo '<td class="',$rcol,'">';
+				echo $rank+$k;
+				echo '</td>',"\n";
+
+				echo '<td class="',$rcol,'">';
+				echo '<a href="index.php?mode=claninfo&amp;clan=',$entry['name'],'">';
+				echo $entry['name'];
+				echo '</a>';
+				echo '</td>',"\n";
+
+				echo '<td class="',$rcol,'">';
+				echo $entry['tag'];
+				echo '</td>',"\n";
+
+				echo '<td class="',$rcol,'">';
+				echo $entry['skill'];
+				echo '</td>',"\n";
+
+				echo '<td class="',$rcol,'">';
+				echo $entry['nummembers'];
+				echo '</td>',"\n";
+				
+				echo '<td class="',$rcol,'">';
+				echo $entry['kills'];
+				echo '</td>',"\n";
+
+				echo '<td class="',$rcol,'">';
+				echo $entry['deaths'];
+				echo '</td>',"\n";
+				
+				echo '<td class="',$rcol,'">';
+				echo number_format($entry['kpd'],1);
+				echo '</td>',"\n";
+
+				echo '</tr>';
+			}
+			echo '<tr><td colspan="8" align="right">';
+				if($clans['pages'] > 1) {
+					for($i=1;$i<=$clans['pages'];$i++) {
+						if($page == ($i)) {
+							echo "[",$i,"]";
+						}
+						else {
+							echo "<a href='index.php?",makeQueryString(array('page'=>$i)),"'>[",$i,"]</a>";
+						}
+					}
+				}
+				else {
+					echo "[1]";
+				}
+		}
+		else {
+			echo '<tr><td colspan="8">',l('No data recorded'),'</td></tr>';
+		}
+	?>
 	</table>
 </div>
-
-<p>
-<form method="GET" action="index.php">
-	<input type="hidden" name="mode" value="search">
-	<input type="hidden" name="game" value="<?php echo $game; ?>">
-	<input type="hidden" name="st" value="clan">
-
-	<table width="90%" align="center" border="0" cellspacing="0" cellpadding="2">
-		<tr valign="bottom">
-			<td width="75%"><?php echo $g_options["font_normal"]; ?><b>&#149;</b> <?php echo l('Find a clan'); ?>: <input type="text" name="q" size=20 maxlength=64 class="textbox"> <input type="submit" value="<?php echo l('Search'); ?>" class="smallsubmit"><?php echo $g_options["fontend_normal"]; ?></td>
-			<td width="25%" align="right" nowrap><?php echo $g_options["font_normal"]; ?><?php echo l('Go to'); ?> <a href="<?php echo "index.php?mode=players&amp;game=$game"; ?>"><img src="<?php echo $g_options["imgdir"]; ?>/player.gif" width="16" height="16" hspace="3" border="0" align="middle" alt="player.gif"><?php echo l('Player Rankings'); ?></a><?php echo $g_options["fontend_normal"]; ?></td>
-		</tr>
-	</table>
-</form>
-</p>
-<?php
-	$table = new Table(
-		array(
-			new TableColumn(
-				"name",
-				"Name",
-				"width=30&icon=clan&link=" . urlencode("mode=claninfo&amp;clan=%k")
-			),
-			new TableColumn(
-				"tag",
-				"Tag",
-				"width=15"
-			),
-			new TableColumn(
-				"skill",
-				"Points",
-				"width=10&align=right"
-			),
-			new TableColumn(
-				"nummembers",
-				"Members",
-				"width=10&align=right"
-			),
-			new TableColumn(
-				"kills",
-				"Kills",
-				"width=10&align=right"
-			),
-			new TableColumn(
-				"deaths",
-				"Deaths",
-				"width=10&align=right"
-			),
-			new TableColumn(
-				"kpd",
-				"Kills per Death",
-				"width=10&align=right"
-			)
-		),
-		"clanId",
-		"skill",
-		"kpd",
-		true
-	);
-
-	$queryClans = mysql_query("
-		SELECT
-			".DB_PREFIX."_Clans.clanId,
-			".DB_PREFIX."_Clans.name,
-			".DB_PREFIX."_Clans.tag,
-			COUNT(".DB_PREFIX."_Players.playerId) AS nummembers,
-			SUM(".DB_PREFIX."_Players.kills) AS kills,
-			SUM(".DB_PREFIX."_Players.deaths) AS deaths,
-			ROUND(AVG(".DB_PREFIX."_Players.skill)) AS skill,
-			IFNULL(SUM(".DB_PREFIX."_Players.kills)/SUM(".DB_PREFIX."_Players.deaths), '-') AS kpd
-		FROM
-			".DB_PREFIX."_Clans
-		LEFT JOIN ".DB_PREFIX."_Players ON
-			".DB_PREFIX."_Players.clan=".DB_PREFIX."_Clans.clanId
-		WHERE
-			".DB_PREFIX."_Clans.game='".mysql_escape_string($game)."'
-			AND ".DB_PREFIX."_Players.hideranking = 0
-		GROUP BY
-			".DB_PREFIX."_Clans.clanId
-		HAVING
-			nummembers >= ".mysql_escape_string($minmembers)."
-		ORDER BY
-			".$table->sort." ".$table->sortorder.",
-			".$table->sort2." ".$table->sortorder.",
-			name ASC
-		LIMIT ".$table->startitem.",".$table->numperpage."
-	");
-
-	$resultquery = mysql_query("
-		SELECT
-			COUNT(*) AS cc
-		FROM
-			".DB_PREFIX."_Clans
-		LEFT JOIN ".DB_PREFIX."_Players ON
-			".DB_PREFIX."_Players.clan=".DB_PREFIX."_Clans.clanId
-		WHERE
-			".DB_PREFIX."_Clans.game='".mysql_escape_string($game)."'
-			AND ".DB_PREFIX."_Players.hideranking = 0
-		GROUP BY
-			".DB_PREFIX."_Clans.clanId
-		HAVING
-			COUNT(".DB_PREFIX."_Players.playerId) >= ".mysql_escape_string($minmembers)."");
-	$result = mysql_fetch_assoc($resultquery);
-	$resultCount = $result['cc'];
-
-	$table->draw($queryClans, $resultCount, 90);
-?>
-<p>
-<form method="GET" action="index.php">
-<input type="hidden" name="game" value="<?php echo $game; ?>" />
-<input type="hidden" name="mode" value="clans" />
-<table width="90%" align="center" border="0" cellspacing="0" cellpadding="2">
-<tr valign="bottom">
-	<td width="75%"><?php echo $g_options["font_normal"]; ?>
-		<b>&#149;</b> <?php echo l('Only show clans with'); ?> <input type="text" name="minmembers" size=4 maxlength=2 value="<?php echo $minmembers; ?>" class="textbox">
-		<?php echo l('or more members'); ?>. <input type="submit" value="<?php echo l('Apply'); ?>" class="smallsubmit"><?php echo $g_options["fontend_normal"]; ?>
-	</td>
-</tr>
-</table>
-</form>
-</p>
