7e835cd9c749536bab9a99a5aee2f8e28eebcd2b
jumpin-banana <jumpin.banana@gmail.com>
2010-06-30 22:05:55 +0800

    manage server: edit and save

--- web/hlstatsinc/admintasks/servers.inc.php
+++ web/hlstatsinc/admintasks/servers.inc.php
@@ -54,28 +54,32 @@ if(isset($_GET['gc'])) {
 	$check = validateInput($gc,'nospace');
 	if($check === true) {
 
-		// update
-		if(!empty($_POST['server']) && !empty($_POST['port'])) {
-			// update given patterns
-			foreach($_POST['server'] as $k=>$v) {
-				$v = trim($v);
-				if(!empty($v) && isset($_POST['server'][$k])) {
-					$query = mysql_query("UPDATE `".DB_PREFIX."_Servers`
-											SET `address` = '".$v."',
-												`port` = '".mysql_escape_string(trim($_POST['port'][$k]))."',
-												`name` = '".mysql_escape_string(trim($_POST['name'][$k]))."',
-												`port` = '".mysql_escape_string(trim($_POST['port'][$k]))."',
-												`port` = '".mysql_escape_string(trim($_POST['port'][$k]))."',
-												`port` = '".mysql_escape_string(trim($_POST['port'][$k]))."',
-												`port` = '".mysql_escape_string(trim($_POST['port'][$k]))."'
-											WHERE `serverId` = '".$k."'");
-					if($query === false) {
-						$return['status'] = "1";
-						$return['msg'] = l('Data could not be saved');
+		if(isset($_POST['sub']['saveServer'])) {
+			// update
+			if(!empty($_POST['server']) && !empty($_POST['port'])) {
+				// update given patterns
+				foreach($_POST['server'] as $k=>$v) {
+					$v = trim($v);
+					if(!empty($v) && isset($_POST['server'][$k])) {
+						$query = mysql_query("UPDATE `".DB_PREFIX."_Servers`
+												SET `address` = '".$v."',
+													`port` = '".mysql_escape_string(trim($_POST['port'][$k]))."',
+													`name` = '".mysql_escape_string(trim($_POST['name'][$k]))."',
+													`game` = '".mysql_escape_string($gc)."',
+													`publicaddress` = '".mysql_escape_string(trim($_POST['pub'][$k]))."',
+													`statusurl` = '".mysql_escape_string(trim($_POST['stat'][$k]))."',
+													`rcon_password` = '".mysql_escape_string(trim($_POST['rcon'][$k]))."',
+													`defaultMap` = '".mysql_escape_string(trim($_POST['map'][$k]))."'
+												WHERE `serverId` = '".$k."'");
+						if($query === false) {
+							$return['status'] = "1";
+							$return['msg'] = l('Data could not be saved');
+						}
 					}
 				}
 			}
 		}
+
 
 		// load the server
 		$query = mysql_query("SELECT s.serverId, s.address, s.port,
@@ -130,72 +134,79 @@ pageHeader(array(l("Admin"),l('Servers')), array(l("Admin")=>"index.php?mode=adm
 		<?php echo l('The Default map is used to sepecify the map if HLStats is unable to determine the map'); ?>.
 	</p>
 	<?php if(!empty($servers)) { ?>
-	<table cellpadding="2" cellspacing="0" border="1" width="100%">
-		<tr>
-			<th>&nbsp;</th>
-			<th><?php echo l('IP Address'); ?> *</th>
-			<th><?php echo l('Port'); ?> *</th>
-			<th><?php echo l('Server Name'); ?> *</th>
-			<th><?php echo l('Delete'); ?></th>
-		</tr>
-	<?php foreach($servers as $s) { ?>
-		<tr>
-			<td class="<?php echo toggleRowClass($rcol); ?>"><img src="hlstatsimg/server.gif" alt="<?php echo l('Server'); ?>" /></td>
-			<td>
-				<input size="10" type="text" name="server[<?php echo $s['serverId']; ?>]" value="<?php echo $s['address']; ?>" />
-			</td>
-			<td class="<?php echo ($rcol); ?>">
-				<input size="5" type="text" name="port[<?php echo $s['serverId']; ?>]" value="<?php echo $s['port']; ?>" />
-			</td>
-			<td class="<?php echo ($rcol); ?>">
-				<input size="25" type="text" name="name[<?php echo $s['serverId']; ?>]" value="<?php echo $s['serverName']; ?>" />
-			</td>
-			<td align="center" class="<?php echo ($rcol); ?>">
-				<input type="checkbox" name="del[<?php echo $s['serverId']; ?>]" value="yes" />
-			</td>
-		</tr>
-		<tr>
-			<td class="<?php echo ($rcol); ?>">&nbsp;</td>
-			<td colspan="3" class="small <?php echo ($rcol); ?>">
-				<b><?php echo l('Rcon Password'); ?> :</b>
-				<input size="10"  type="text" name="rcon[<?php echo $s['serverId']; ?>]" value="<?php echo $s['rcon_password']; ?>" />&nbsp;
-				&nbsp;
-				<b><?php echo l('Default Server Map'); ?> :</b>
-				<input size="10"  type="text" name="map[<?php echo $s['serverId']; ?>]" value="<?php echo $s['defaultMap']; ?>" />
-			</td>
-			<td class="<?php echo ($rcol); ?>">&nbsp;</td>
-		</tr>
-	<?php } ?>
-		<tr>
-			<td class="<?php echo toggleRowClass($rcol); ?>"><?php echo l('new'); ?></td>
-			<td class="<?php echo ($rcol); ?>">
-				<input size="10" type="text" name="newIP" value="" />
-			</td>
-			<td class="<?php echo ($rcol); ?>">
-				<input size="5" type="text" name="newport" value="" />
-			</td>
-			<td class="<?php echo ($rcol); ?>">
-				<input size="25" type="text" name="newname" value="" />
-			</td>
-		</tr>
-		<tr>
-			<td class="<?php echo ($rcol); ?>">&nbsp;</td>
-			<td colspan="3" class="small <?php echo ($rcol); ?>">
-				<b><?php echo l('Rcon Password'); ?> :</b>
-				<input size="10"  type="text" name="newrcon" value="" />&nbsp;
-				&nbsp;
-				<b><?php echo l('Default Server Map'); ?> :</b>
-				<input size="10"  type="text" name="newmap" value="" />
-			</td>
-			<td class="<?php echo ($rcol); ?>">&nbsp;</td>
-		</tr>
-		<tr>
-			<td colspan="5" align="right">
-				<button type="submit" name="sub[saveServer]" title="<?php echo l('Save'); ?>">
-					<?php echo l('Save'); ?>
-				</button>
-			</td>
-		</tr>
-	</table>
+	<form method="post" action="">
+		<table cellpadding="2" cellspacing="0" border="1" width="100%">
+			<tr>
+				<th>&nbsp;</th>
+				<th><?php echo l('IP Address'); ?> *</th>
+				<th><?php echo l('Port'); ?> *</th>
+				<th><?php echo l('Server Name'); ?> *</th>
+				<th><?php echo l('Delete'); ?></th>
+			</tr>
+		<?php foreach($servers as $s) { ?>
+			<tr>
+				<td class="<?php echo toggleRowClass($rcol); ?>"><img src="hlstatsimg/server.gif" alt="<?php echo l('Server'); ?>" /></td>
+				<td>
+					<input size="10" type="text" name="server[<?php echo $s['serverId']; ?>]" value="<?php echo $s['address']; ?>" />
+				</td>
+				<td class="<?php echo ($rcol); ?>">
+					<input size="5" type="text" name="port[<?php echo $s['serverId']; ?>]" value="<?php echo $s['port']; ?>" />
+				</td>
+				<td class="<?php echo ($rcol); ?>">
+					<input size="25" type="text" name="name[<?php echo $s['serverId']; ?>]" value="<?php echo $s['serverName']; ?>" />
+				</td>
+				<td align="center" class="<?php echo ($rcol); ?>">
+					<input type="checkbox" name="del[<?php echo $s['serverId']; ?>]" value="yes" />
+				</td>
+			</tr>
+			<tr>
+				<td class="<?php echo ($rcol); ?>">&nbsp;</td>
+				<td colspan="3" class="small <?php echo ($rcol); ?>">
+					<b><?php echo l('Rcon Password'); ?> :</b>
+					<input size="10"  type="text" name="rcon[<?php echo $s['serverId']; ?>]" value="<?php echo $s['rcon_password']; ?>" />&nbsp;
+					&nbsp;
+					<b><?php echo l('Default Server Map'); ?> :</b>
+					<input type="text" name="map[<?php echo $s['serverId']; ?>]" value="<?php echo $s['defaultMap']; ?>" /><br />
+					<b><?php echo l('Public Address'); ?> :</b>
+					<input type="text" name="pub[<?php echo $s['serverId']; ?>]" value="<?php echo $s['publicaddress']; ?>" />&nbsp;
+					&nbsp;
+					<b><?php echo l('Status URL'); ?> :</b>
+					<input type="text" name="stat[<?php echo $s['serverId']; ?>]" value="<?php echo $s['statusurl']; ?>" />
+				</td>
+				<td class="<?php echo ($rcol); ?>">&nbsp;</td>
+			</tr>
+		<?php } ?>
+			<tr>
+				<td class="<?php echo toggleRowClass($rcol); ?>"><?php echo l('new'); ?></td>
+				<td class="<?php echo ($rcol); ?>">
+					<input size="10" type="text" name="newIP" value="" />
+				</td>
+				<td class="<?php echo ($rcol); ?>">
+					<input size="5" type="text" name="newport" value="" />
+				</td>
+				<td class="<?php echo ($rcol); ?>">
+					<input size="25" type="text" name="newname" value="" />
+				</td>
+			</tr>
+			<tr>
+				<td class="<?php echo ($rcol); ?>">&nbsp;</td>
+				<td colspan="3" class="small <?php echo ($rcol); ?>">
+					<b><?php echo l('Rcon Password'); ?> :</b>
+					<input size="10"  type="text" name="newrcon" value="" />&nbsp;
+					&nbsp;
+					<b><?php echo l('Default Server Map'); ?> :</b>
+					<input size="10"  type="text" name="newmap" value="" />
+				</td>
+				<td class="<?php echo ($rcol); ?>">&nbsp;</td>
+			</tr>
+			<tr>
+				<td colspan="5" align="right">
+					<button type="submit" name="sub[saveServer]" title="<?php echo l('Save'); ?>">
+						<?php echo l('Save'); ?>
+					</button>
+				</td>
+			</tr>
+		</table>
+	</form>
 	<?php } ?>
 </div>
